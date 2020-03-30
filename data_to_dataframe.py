d118ab687de7403e32abb899cc4e351ffd0dd772
WybeKoper <WybeKoper@users.noreply.github.com>
2020-03-30 14:37:09 +0200

    Fixed error

---
+++ data_to_dataframe.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import json
+
+with open('C:/Users/wybek/Documents/school/Master/Information Retrieval/project2/data/relevant_Tables.json', 'r') as file:
+    data = json.load(file)
+    for i in data:
+        print(data)
+
--- wordvec.py
+++
@@ -1,3 +0,0 @@
-import gensim
-
-model = gensim.models.Word2Vec.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)  
