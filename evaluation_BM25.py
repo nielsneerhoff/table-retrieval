698042c63c3f216ab8a8a0d15d1eec0d828481ad
Niels Neerhoff <n.neerhoff@student.tudelft.nl>
2020-04-07 22:09:27 +0200

    Rm index from search.py, rm limit.

--- evaluation_BM25.py
+++ evaluation_BM25.py
@@ -1,12 +1,12 @@
-from whoosh.index import open_dir
 from sklearn.metrics import ndcg_score
 import pandas as pd
 import pickle
-from whoosh import qparser
+from whoosh.index import open_dir
 
-from parser import DEFAULT_FIELD_PARSER
 from search import search_bm25f, DUMMY_BM25F
+from index import INDEX_NAME
 
+INDEX = open_dir(INDEX_NAME)
 
 queries = pd.read_csv('./data/queries.csv')
 queries = queries['query'].tolist()
@@ -14,11 +14,6 @@ queries = [item.strip() for item in queries]
 
 with open('./data/qrels_dict.pickle', 'rb') as handle:
     qrels = pickle.load(handle)
-
-# schema = Schema(id=TEXT(stored=True), title=TEXT(stored=True), numCols=NUMERIC(stored=True), pgTitle=TEXT(stored=True), numDataRows=NUMERIC(stored=True), secondTitle=TEXT(stored=True), numHeaderRows=NUMERIC(stored=True), caption=TEXT(stored=True), content=TEXT(stored=True))
-
-ix = open_dir("index")
-mparser = DEFAULT_FIELD_PARSER
 
 total_ndcg_5 = 0
 total_ndcg_10 = 0
@@ -28,8 +23,7 @@ for index, query in enumerate(queries):
     query_number = index + 1
     query_string = str(query)
 
-    results = search_bm25f(query_string, DUMMY_BM25F)
-
+    results = search_bm25f(query_string, DUMMY_BM25F, INDEX)
     qrels_one_query = qrels[query_number]
 
     predicted_relevance = list()
@@ -40,10 +34,14 @@ for index, query in enumerate(queries):
             actual_relevance.append(qrels_one_query[table['id']])
 
     if(len(actual_relevance) > 1):
-        total_ndcg_5 += ndcg_score([actual_relevance], [predicted_relevance], k = 5)
-        total_ndcg_10 += ndcg_score([actual_relevance], [predicted_relevance],k = 10)
-        total_ndcg_15 += ndcg_score([actual_relevance], [predicted_relevance], k = 15)
-        total_ndcg_20 += ndcg_score([actual_relevance], [predicted_relevance], k = 20)
+        total_ndcg_5 += ndcg_score(
+            [actual_relevance], [predicted_relevance], k = 5)
+        total_ndcg_10 += ndcg_score(
+            [actual_relevance], [predicted_relevance], k = 10)
+        total_ndcg_15 += ndcg_score(
+            [actual_relevance], [predicted_relevance], k = 15)
+        total_ndcg_20 += ndcg_score(
+            [actual_relevance], [predicted_relevance], k = 20)
 
 
 print("average ndcg @ 5 score = " + str(total_ndcg_5 / 60))
--- search.py
+++ search.py
@@ -1,12 +1,6 @@
 from whoosh import scoring
 from whoosh import searching
 
-# Delete wanneer we vanuit andere file gaan aanroepen.
-from whoosh.index import open_dir
-from index import INDEX_NAME
-INDEX = open_dir(INDEX_NAME)
-# ---------------------------------------------------
-
 from parser import SINGLE_FIELD_PARSER, MULTI_FIELD_PARSER, DEFAULT_FIELD_PARSER
 
 def BM25F(titles_b, caption_and_headers_b, cells_b):
@@ -22,36 +16,32 @@ def BM25F(titles_b, caption_and_headers_b, cells_b):
 DUMMY_BM25F = BM25F(0.5, 0.75, 0.5)
 DUMMY_BM25 = scoring.BM25F
 
-def search(query_string, scoring_function, query_parser, index = INDEX):
+def search(query_string, scoring_function, query_parser, index):
     """ Search index using a scoring function and a query parser. Limits search results to limit. """
 
-    # TODO Add limit?
     with index.searcher(weighting = scoring_function) as searcher:
         query = query_parser.parse(query_string)
         results = searcher.search(query, limit = 3120)
         print(len(results), 'results found for', query_string)
         return results
 
-def search_bm25f(query_string, scoring_function, index = INDEX):
+def search_bm25f(query_string, scoring_function, index):
     """ Search index using bm25f scoring function and composite multi-field parser. Limits search results to limit. """
 
-    return search(query_string, scoring_function, MULTI_FIELD_PARSER)
+    return search(query_string, scoring_function, MULTI_FIELD_PARSER, index)
 
-def search_single_field(
-    query_string, scoring_function, index = INDEX):
+def search_single_field(query_string, scoring_function, index):
     """ Search index using bm25f scoring function and composite multi-field parser. Limits search results to limit. """
 
-    return search(query_string, scoring_function, SINGLE_FIELD_PARSER)
+    return search(query_string, scoring_function, SINGLE_FIELD_PARSER, index)
 
-def search_multi_field(
-    query_string, scoring_function, index = INDEX):
+def search_multi_field(query_string, scoring_function, index):
     """ Search index using bm25f scoring function and multi-field parser. Limits search results to limit. """
 
     # To be implemented.
     pass
 
-def search_default(
-    query_string, scoring_function = scoring.BM25F, index = INDEX):
+def search_default(query_string, index):
     """ Search index using bm25f default scoring function and base (default) parser. Limits search results to limit. """
 
-    return search(query_string, scoring_function, DEFAULT_FIELD_PARSER)
+    return search(query_string, scoring.BM25F, DEFAULT_FIELD_PARSER, index)
