302060afec2ac76f20cfe6c87ba6dad015ee51f6
Niels Neerhoff <n.neerhoff@student.tudelft.nl>
2020-04-07 22:01:43 +0200

    Adapted to search methods from search.py

--- evaluation_BM25.py
+++ evaluation_BM25.py
@@ -1,13 +1,11 @@
 from whoosh.index import open_dir
-from whoosh.qparser import QueryParser
-from whoosh.qparser import MultifieldParser
-from whoosh.query import *
-from whoosh.fields import *
 from sklearn.metrics import ndcg_score
 import pandas as pd
-from whoosh import scoring
 import pickle
 from whoosh import qparser
+
+from parser import DEFAULT_FIELD_PARSER
+from search import search_bm25f, DUMMY_BM25F
 
 
 queries = pd.read_csv('./data/queries.csv')
@@ -17,42 +15,35 @@ queries = [item.strip() for item in queries]
 with open('./data/qrels_dict.pickle', 'rb') as handle:
     qrels = pickle.load(handle)
 
-schema = Schema(id=TEXT(stored=True), title=TEXT(stored=True), numCols=NUMERIC(stored=True), pgTitle=TEXT(stored=True), numDataRows=NUMERIC(stored=True), secondTitle=TEXT(stored=True), numHeaderRows=NUMERIC(stored=True), caption=TEXT(stored=True), content=TEXT(stored=True))
+# schema = Schema(id=TEXT(stored=True), title=TEXT(stored=True), numCols=NUMERIC(stored=True), pgTitle=TEXT(stored=True), numDataRows=NUMERIC(stored=True), secondTitle=TEXT(stored=True), numHeaderRows=NUMERIC(stored=True), caption=TEXT(stored=True), content=TEXT(stored=True))
 
 ix = open_dir("index")
-mparser = MultifieldParser(["title", "content", "pgTitle", "caption", "secondTitle"], schema=schema, group=qparser.OrGroup)
+mparser = DEFAULT_FIELD_PARSER
+
 total_ndcg_5 = 0
 total_ndcg_10 = 0
 total_ndcg_15 = 0
 total_ndcg_20 = 0
 for index, query in enumerate(queries):
     query_number = index + 1
+    query_string = str(query)
 
-    with ix.searcher(weighting=scoring.BM25F) as searcher:
-        querystring = str(query)
-        print(str(query_number) + " " + querystring)
-        myquery = mparser.parse(querystring)
+    results = search_bm25f(query_string, DUMMY_BM25F)
 
-        results = searcher.search(myquery)
-        print("Retrieved: " + str(len(results)) + " tables")
-        qrels_one_query = qrels[query_number]
+    qrels_one_query = qrels[query_number]
 
-        predicted_relevance = list()
-        actual_relevance = list()
-        for table in results:
-            #print('Table id: ' + str(table['id']) + ' BM25 score: ' + str(table.score))
-            if(table['id'] in qrels_one_query):
-                predicted_relevance.append(table.score)
-                actual_relevance.append(qrels_one_query[table['id']])
-        #print(predicted_relevance)
-        #print(actual_relevance)
+    predicted_relevance = list()
+    actual_relevance = list()
+    for table in results:
+        if(table.docnum in qrels_one_query):
+            predicted_relevance.append(table.score)
+            actual_relevance.append(qrels_one_query[table['id']])
 
-        if(len(actual_relevance) > 1):
-            total_ndcg_5 += ndcg_score([actual_relevance], [predicted_relevance], k = 5)
-            total_ndcg_10 += ndcg_score([actual_relevance], [predicted_relevance], k = 10)
-            total_ndcg_15 += ndcg_score([actual_relevance], [predicted_relevance], k = 15)
-            total_ndcg_20 += ndcg_score([actual_relevance], [predicted_relevance], k = 20)
-            #print("ndcg: " + str(ndcg))
+    if(len(actual_relevance) > 1):
+        total_ndcg_5 += ndcg_score([actual_relevance], [predicted_relevance], k = 5)
+        total_ndcg_10 += ndcg_score([actual_relevance], [predicted_relevance],k = 10)
+        total_ndcg_15 += ndcg_score([actual_relevance], [predicted_relevance], k = 15)
+        total_ndcg_20 += ndcg_score([actual_relevance], [predicted_relevance], k = 20)
 
 
 print("average ndcg @ 5 score = " + str(total_ndcg_5 / 60))
--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from whoosh.qparser import MultifieldParser
+from whoosh.qparser import MultifieldParser, OrGroup
 
 from schema import SCHEMA
 
@@ -7,7 +7,7 @@ SINGLE_FIELD_PARSER = MultifieldParser(["all_concatenated"], schema = SCHEMA)
 
 # For BM25F
 MULTI_FIELD_PARSER = MultifieldParser(
-    ['titles', 'caption_and_headers', 'cells'], schema = SCHEMA)
+    ['titles', 'caption_and_headers', 'cells'], schema = SCHEMA, group = OrGroup)
 
 DEFAULT_FIELD_PARSER = MultifieldParser(
-    ["title", "content", "pgTitle", "caption", "secondTitle"], schema = SCHEMA)
+    ["title", "content", "pgTitle", "caption", "secondTitle"], schema = SCHEMA, group = OrGroup)
