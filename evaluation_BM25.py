98cad394f91c2e6c5f71b2102cece734bd001d53
Niels Neerhoff <n.neerhoff@student.tudelft.nl>
2020-04-08 10:06:41 +0200

    Added composite fields, cleaned all fields, evaluated ndcg again.

--- evaluation_BM25.py
+++ evaluation_BM25.py
@@ -1,12 +1,8 @@
 from sklearn.metrics import ndcg_score
 import pandas as pd
 import pickle
-from whoosh.index import open_dir
 
-from search import search_bm25f, DUMMY_BM25F
-from index import INDEX_NAME
-
-INDEX = open_dir(INDEX_NAME)
+from search import search_bm25f, search_single_field, BM25F
 
 queries = pd.read_csv('./data/queries.csv')
 queries = queries['query'].tolist()
@@ -15,36 +11,59 @@ queries = [item.strip() for item in queries]
 with open('./data/qrels_dict.pickle', 'rb') as handle:
     qrels = pickle.load(handle)
 
-total_ndcg_5 = 0
-total_ndcg_10 = 0
-total_ndcg_15 = 0
-total_ndcg_20 = 0
-for index, query in enumerate(queries):
-    query_number = index + 1
-    query_string = str(query)
+def evaluate(search_function, scoring_function):
+    """ Evaluates a search function using scoring function. See search.py for different search functions. """
+    # Initialize NDCG's at zero.
+    total_ndcg_5, total_ndcg_10, total_ndcg_15, total_ndcg_20 = 0, 0, 0, 0
 
-    results = search_bm25f(query_string, DUMMY_BM25F, INDEX)
-    qrels_one_query = qrels[query_number]
+    for query_index, query_string in enumerate(queries):
+        qrels_one_query = qrels[query_index + 1]
 
-    predicted_relevance = list()
-    actual_relevance = list()
-    for table in results:
-        if(table.docnum in qrels_one_query):
-            predicted_relevance.append(table.score)
-            actual_relevance.append(qrels_one_query[table['id']])
+        # Execute the search function with query, scoring function.
+        results, searcher = search_function(query_string, scoring_function)
 
-    if(len(actual_relevance) > 1):
-        total_ndcg_5 += ndcg_score(
-            [actual_relevance], [predicted_relevance], k = 5)
-        total_ndcg_10 += ndcg_score(
-            [actual_relevance], [predicted_relevance], k = 10)
-        total_ndcg_15 += ndcg_score(
-            [actual_relevance], [predicted_relevance], k = 15)
-        total_ndcg_20 += ndcg_score(
-            [actual_relevance], [predicted_relevance], k = 20)
+        predicted_relevance = []
+        actual_relevance = []
+        for table in results:
+            if(table['id'] in qrels_one_query):
+                predicted_relevance.append(table.score)
+                actual_relevance.append(qrels_one_query[table['id']])
 
+        searcher.close()
 
-print("average ndcg @ 5 score = " + str(total_ndcg_5 / 60))
-print("average ndcg @ 10 score = " + str(total_ndcg_10 / 60))
-print("average ndcg @ 15 score = " + str(total_ndcg_15 / 60))
-print("average ndcg @ 20 score = " + str(total_ndcg_20 / 60))
+        if(len(actual_relevance) > 1):
+            total_ndcg_5 += ndcg_score(
+                [actual_relevance], [predicted_relevance], k = 5)
+            total_ndcg_10 += ndcg_score(
+                [actual_relevance], [predicted_relevance], k = 10)
+            total_ndcg_15 += ndcg_score(
+                [actual_relevance], [predicted_relevance], k = 15)
+            total_ndcg_20 += ndcg_score(
+                [actual_relevance], [predicted_relevance], k = 20)
+
+    return {
+        'ndcg_at_5' : total_ndcg_5 / 60, 
+        'ndcg_at_10' : total_ndcg_10 / 60,
+        'ndcg_at_15' : total_ndcg_15 / 60,
+        'ndcg_at_20' : total_ndcg_20 / 60
+        # TODO Add MAP, and others?
+        }
+
+def hyper_parameter_evaluate():
+    for i in range(1, 10):
+        titles_b = i / 10
+        for j in range(1, 10):
+            caption_and_headers = j / 10
+            for k in range(1, 10):
+                cells_b = k / 10
+                # BM25F
+                scoring_function = BM25F(
+                    titles_b, caption_and_headers, cells_b)
+                search_function = search_bm25f
+                # Single-field BM25
+                # scoring_function = BM25F()
+                # search_function = search_single_field
+                result = evaluate(search_function, scoring_function)
+                print(i, j, k, result)
+
+hyper_parameter_evaluate()
--- schema.py
+++ schema.py
@@ -1,36 +1,42 @@
 from whoosh.fields import TEXT, NUMERIC, Schema
 from whoosh.index import open_dir
+from re import sub
+
+# QUESTIONS TO DISCUSS (IN REPORT)
+# 1. Do we want to keep doubles or stick with unique values?
+# 2. Do we want to remove hyperlinks?
+# 3. Do we want to lemmatize/stem? (Seems logical to do)
+# 4. Why do we get better NDCG results without cleaning?
+
+# Example cleaned field:
+# Denotes players who are currently on the Bobcats roster Denotes players who are currently on the Bobcats roster Denotes players who are currently on the Bobcats roster Denotes players who are currently on the Bobcats roster Denotes players who are currently on the Bobcats roster No No Jersey number Jersey number Pos Position Position G Basketball positions Basketball positions F Basketball positions C Basketball positions Pts Point basketball Point basketball Reb Rebound basketball Ast Assist basketball
 
 SCHEMA = Schema(
     id = TEXT(stored=True),
-    title = TEXT(stored=True),
+    headers = TEXT(stored=True),
     numCols = NUMERIC(stored=True),
-    pgTitle = TEXT(stored=True),
+    page_title = TEXT(stored=True),
     numDataRows = NUMERIC(stored=True),
-    secondTitle = TEXT(stored=True),
+    section_title = TEXT(stored=True),
     numHeaderRows = NUMERIC(stored=True),
     caption = TEXT(stored=True),
-    content = TEXT(stored=True),
+    body = TEXT(stored=True),
     titles = TEXT(stored=True),
     caption_and_headers = TEXT(stored=True),
-    cells = TEXT(stored=True),
     all_concatenated = TEXT(stored = True))
 
 def to_fields(identifier, table):
     fields = {}
 
-    # TODO: Strip fields from array signs etc ?
-
     # Text values.
-    fields['title'] = str(table['title'])
-    fields['pgTitle'] = str(table['pgTitle'])
-    fields['secondTitle'] = str(table['secondTitle'])
-    fields['caption'] = str(table['caption'])
-    fields['content'] = str(table['data'])
-    fields['titles'] = fields['pgTitle'] + ' ' + fields['title']
-    fields['caption_and_headers'] = fields['caption'] + ' ' # TODO
-    fields['cells'] = '' # TODO
-    fields['all_concatenated'] = fields['titles'] + fields['caption_and_headers'] + fields['cells']
+    fields['headers'] = clean_list(table['title'])
+    fields['page_title'] = clean_string(table['pgTitle'])
+    fields['section_title'] = clean_string(table['secondTitle'])
+    fields['caption'] = clean_string(table['caption'])
+    fields['body'] = clean_matrix(table['data'])
+    fields['titles'] = fields['page_title'] + ' ' + fields['section_title']
+    fields['caption_and_headers'] = fields['caption'] + ' ' + fields['headers']
+    fields['all_concatenated'] = fields['titles'] + fields['caption_and_headers'] + fields['body']
     
     # Numeric values.
     fields['id'] = identifier
@@ -39,3 +45,32 @@ def to_fields(identifier, table):
     fields['numHeaderRows'] = int(table['numHeaderRows'])
 
     return fields
+
+def clean_string(string):
+    """ Cleans a string from special characters, numbers and hyperlinks. """
+    if string:
+        string = sub('[^A-Za-z0-9]+', ' ', string.split('|', 1)[0])
+    return string.strip()
+
+def clean_list(array):
+    """ Returns a string with concatenated cleaned strings of entire array. """
+    string = ''
+    for i in range(len(array) - 1):
+        string += clean_string(array[i])
+        if string:
+            string += ' '
+    if len(array) - 1 > -1:
+        string += clean_string(array[len(array) - 1])
+    return string
+
+def clean_matrix(matrix):
+    """ Returns a string with concatenated cleaned strings of entire matrix. """
+    string = ''
+    for i in range(len(matrix) - 1):
+        row = matrix[i]
+        string += clean_list(row)
+        if string:
+            string += ' '
+    if len(matrix) - 1 > -1:
+        string += clean_list(matrix[len(matrix) - 1])
+    return string
