4f15cb73af43c5474c509272229730f15af6a97d
Niels Neerhoff <n.neerhoff@student.tudelft.nl>
2020-04-08 12:08:05 +0200

    Push voor Wybe.

--- evaluation_BM25.py
+++ evaluation_BM25.py
@@ -1,8 +1,9 @@
 from sklearn.metrics import ndcg_score
 import pandas as pd
 import pickle
+from whoosh.scoring import BM25F
 
-from search import search_bm25f, search_single_field, BM25F
+from search import search_bm25f, search_single_field
 
 queries = pd.read_csv('./data/queries.csv')
 queries = queries['query'].tolist()
@@ -41,29 +42,25 @@ def evaluate(search_function, scoring_function):
             total_ndcg_20 += ndcg_score(
                 [actual_relevance], [predicted_relevance], k = 20)
 
-    return {
-        'ndcg_at_5' : total_ndcg_5 / 60, 
-        'ndcg_at_10' : total_ndcg_10 / 60,
-        'ndcg_at_15' : total_ndcg_15 / 60,
-        'ndcg_at_20' : total_ndcg_20 / 60
-        # TODO Add MAP, and others?
-        }
+    return total_ndcg_5 / 60, total_ndcg_10 / 60, total_ndcg_15 / 60, total_ndcg_20 / 60
 
 def hyper_parameter_evaluate():
-    for i in range(1, 10):
-        titles_b = i / 10
-        for j in range(1, 10):
-            caption_and_headers = j / 10
-            for k in range(1, 10):
-                cells_b = k / 10
-                # BM25F
-                scoring_function = BM25F(
-                    titles_b, caption_and_headers, cells_b)
-                search_function = search_bm25f
-                # Single-field BM25
-                # scoring_function = BM25F()
-                # search_function = search_single_field
-                result = evaluate(search_function, scoring_function)
-                print(i, j, k, result)
+    for k in range(25, 300, 25):
+        k = k / 100
+        # Single-field BM25
+        # scoring_function = BM25F(k)
+        # search_function = search_single_field
+        # result = evaluate(search_function, scoring_function)
+        for tb in range(1, 10):
+            titles_b = tb / 10
+            for cb in range(1, 10):
+                caption_and_headers_b = cb / 10
+                for bb in range(1, 10):
+                    body_b = bb / 10
+                    # BM25F
+                    scoring_function = BM25F(K1 = k, titles_B = titles_b, caption_and_headers_B = caption_and_headers_b, body_B = body_b)
+                    search_function = search_bm25f
+                    result = evaluate(search_function, scoring_function)
+                    print(k, titles_b, caption_and_headers_b, body_b, result)
 
 hyper_parameter_evaluate()
--- search.py
+++ search.py
@@ -5,20 +5,6 @@ from whoosh.index import open_dir
 from index import INDEX_NAME
 from parser import SINGLE_FIELD_PARSER, MULTI_FIELD_PARSER, DEFAULT_FIELD_PARSER
 
-def BM25F(titles_b, caption_and_headers_b, cells_b):
-    """ Returns a BM25F scoring function with B parameter configuration specified in arguments. """
-
-    return scoring.BM25F(
-        titles_B = titles_b,
-        cells_b = cells_b,
-        caption_and_headers_b = caption_and_headers_b
-    )
-
-def BM25F():
-    """ Returns a default BM25F scoring function. """
-
-    return scoring.BM25F()
-
 def search(query_string, scoring_function, query_parser, index_name):
     """ Search index using a scoring function and a query parser. Limits search results to limit. """
 
