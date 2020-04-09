042c098e6f437477675f7d6681f0bca527ee5813
Berend-Jan Lange <lange.berendjan@gmail.com>
2020-04-09 14:19:11 +0200

    main static method

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from in_out import InOut
 from feature_set import extract_features
 
-io = InOut()
-features = io.read_features()
+features = InOut.read_features()
 wiki_tables_features = extract_features(features, 'wiki')
 web_tables_features = extract_features(features, 'web')
 ltr_features = extract_features(features, 'ltr')
 str_features = extract_features(features, 'str')
 
-queries = io.read_queries()
+queries = InOut.read_queries()
