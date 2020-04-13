965c191e3af76d4845a426dd96fb84eda879e2e2
Berend-Jan Lange <lange.berendjan@gmail.com>
2020-04-13 21:44:37 +0200

    push

---
+++ extract_semantic_dictionaries.py
@@ -0,0 +1,151 @@
+from in_out import InOut as IO
+base_path_dicts = './data/dictionaries/'
+from extract_semantic_features import set_representation, get_entities_regex
+from math import log
+import wikipedia
+
+def create_word2vec_model(bin_model, words):
+    word2vec = {}
+    for word in bin_model.wv.index2word:
+        if word.lower() in words:
+            word2vec[word.lower()] = bin_model[word].tolist()
+    IO.write_json(word2vec, base_path_dicts + 'word2vec.json')
+    return word2vec
+
+def create_rdf2vec_model(large_model, entities):
+    rdf2vec = {}
+    for word in large_model.keys():
+        if word.lower() in entities:
+            rdf2vec[word.lower()] = large_model[word]
+    IO.write_json(rdf2vec, base_path_dicts + 'rdf2vec.json')
+    return rdf2vec
+
+
+def get_all_words(queries, tables):
+    query_to_words = {}
+    table_to_words = {}
+    all_words = set()
+
+    for q_id, query in queries.items():
+        words_query = set_representation(query, 'words')
+        query_to_words[q_id] = {'all_words' : words_query}
+        all_words = all_words.union(words_query)
+
+    for t_id, table in tables.items():
+        words_table = set_representation(table, 'words')
+        table_to_words[t_id] = {'all_words' : words_table}
+        all_words = all_words.union(words_table)
+
+    get_TFIDF(queries, tables, query_to_words, table_to_words)
+
+    return all_words, query_to_words, table_to_words
+
+
+def get_all_entities(queries, tables):
+    query_to_entities = {}
+    table_to_entities = {}
+    all_entities = set()
+
+    for q_id, query in queries.items():
+        entities_query = find_all_entities_in_query(query)
+        # entities_query = set_representation(query, 'entities')
+        query_to_entities[q_id] = {'all_entities' : list(entities_query)}
+        for entity in entities_query:
+            query_to_entities[q_id][entity] = {}
+        all_entities = all_entities.union(entities_query)
+    IO.write_json(query_to_entities, base_path_dicts + 'query_to_entities.json')
+    
+        
+    for t_id, table in tables.items():
+        entities_table = set_representation(table, 'entities')
+        table_to_entities[t_id] = {'all_entities' : list(entities_table)}
+        for entity in entities_table:
+            table_to_entities[t_id][entity] = {}
+        all_entities = all_entities.union(entities_table)
+    IO.write_json(table_to_entities, base_path_dicts + 'table_to_entities.json')
+
+    return all_entities, query_to_entities, table_to_entities
+
+
+def get_TFIDF(queries, tables, query_to_words, table_to_words):
+    for q_id, query in queries.items():
+        words_query = query_to_words[q_id]['all_words']
+        for word in words_query:
+            query_to_words[q_id][word] = {
+                'word' : word, 
+                'TF' : len([x for x in words_query if x == word]) / len(words_query),
+                'IDF' : log(len(queries) / len([word for x in query_to_words.values() if word in x['all_words']]))
+                }
+            query_to_words[q_id][word]['TFIDF'] = \
+            query_to_words[q_id][word]['TF'] / query_to_words[q_id][word]['IDF']
+            query_to_words[q_id]['all_words'] = list(set(query_to_words[q_id]['all_words']))
+    IO.write_json(query_to_words, base_path_dicts + 'query_to_words.json')
+
+    for t_id, table in tables.items():
+        words_table = table_to_words[t_id]['all_words']
+        for word in words_table:
+            table_to_words[t_id][word] = {
+                'word' : word, 
+                'TF' : len([x for x in words_table if x == word]) / len(words_table),
+                'IDF' : log(len(tables) / len([word for x in table_to_words.values() if word in x['all_words']]))
+                }
+            table_to_words[t_id][word]['TFIDF'] = \
+            table_to_words[t_id][word]['TF'] / table_to_words[t_id][word]['IDF']
+            table_to_words[t_id]['all_words'] = list(set(table_to_words[t_id]['all_words']))
+    IO.write_json(table_to_words, base_path_dicts + 'table_to_words.json')
+
+
+def get_all_words_from_json(query_to_words, table_to_words):
+    all_words = set()
+    for query in query_to_words.values():
+        all_words = all_words.union(query['all_words'])
+    for table in table_to_words.values():
+        all_words = all_words.union(table['all_words'])
+    return all_words
+
+
+def get_all_entities_from_json(query_to_entities, table_to_entities):
+    all_entities = set()
+    for query in query_to_entities.values():
+        all_entities = all_entities.union(query['all_entities'])
+    for table in table_to_entities.values():
+        all_entities = all_entities.union(table['all_entities'])
+    return all_entities
+
+
+def find_all_entities_in_query(query):
+    query_to_ngrams = {}
+
+    splitted = query.split(' ')
+    query_to_ngrams[splitted[0]] = list_to_ngrams(splitted[1:])
+
+    query_to_entities = {}
+    entities = []
+
+    for k, v in query_to_ngrams.items():
+        query_entities = [check_if_entity(i) for i in v if check_if_entity(i) is not None]
+        query_to_entities[k] = sorted(list(set(query_entities)))
+        entities += query_entities
+
+    return entities, query_to_entities
+
+
+def check_if_entity(s):
+    try:
+        page = wikipedia.page(s)
+        res = page.url.split('/')[-1]
+        if res.lower().replace('_', ' ') == s:
+            return res
+        else:
+            return
+    except wikipedia.PageError:
+        return
+    except wikipedia.DisambiguationError:
+        return
+
+
+def list_to_ngrams(words):
+    words_incl_n_grams = []
+    for N in range(1, len(words) + 1):
+        words_incl_n_grams += [' '.join(words[i:i+N]).strip() for i in range(len(words)-N+1)]
+    return words_incl_n_grams
--- test.py
+++ test.py
@@ -1,6 +1,151 @@
-import re
-string = '[List_of_countries_by_GDP_PPP|GDP (PPP)]1314324134-[3443431sad sd|ad]'
+# import re
+# string = '[List_of_countries_by_GDP_PPP|GDP (PPP)]1314324134-[3443431sad sd|ad]'
 
-te = re.compile( r'(\[.*?\|.*?\])').findall(string)
+# print(re.findall('(\[.*?\|.*?\])', string))
+# for s in re.findall('(\[.*?\|.*?\])', string):
+#     string = string.replace(s, s.split('|')[1].split(']')[0])
+# print(string)
 
-print(te)
+# st2 = re.findall('(\[.*?\|.*?\])', string)
+# print(st2)
+# print(string)
+# for s in st2:
+#     s3 = 3
+#     string = string.replace(s, s.split('|')[1].split(']')[0])
+#     print(string)
+#     print()
+
+# se2 = string.map(lambda x: re.sub(x, x.split('|')[1]), st)
+
+# print(se2)
+
+# se2 = [0] * 8
+# query_to_words = {
+#     '1' : {'words' : {'et', 'as', 'lf'}},
+#     '3' : {'words' : {'as', 'sd'}},
+#     '4' : {'words' : {'as', 'er'}}
+# }
+
+# sad = {'ear' : {}}
+# sad['ear']['asdsa'] = 2
+# print(sad)
+
+# print(query_to_words.values())
+# for word in ['er', 'et', 'as', 've']:
+#     print(len([word for x in query_to_words.values() if word in x['words']]))
+
+# a = {
+#     'a',
+#     'b',
+#     'd'
+#     }
+# b = {
+#     'a' : 1,
+#     'b' : 2,
+#     'c' : 3
+#     }
+
+# s = {'er', 'et', 'as', 've'}
+
+# query = {
+#     'words' : list(s),
+#     'er' : {
+#         'TFIDF' : 2
+#     },
+#     'et' : {
+#         'TFIDF' : 24
+#     },
+#     'as' : {
+#         'TFIDF' : 26
+#     },
+#     've' : {
+#         'TFIDF' : 4
+#     }
+# }
+
+# query_tfidf = list(map(lambda x: query[x]['TFIDF'], query['words']))
+# print(query_tfidf)
+# print(list(zip(a,b)))
+
+# print(dict(map(lambda x: x, a)))
+
+# x = set(['e', 'f'])
+# y = set(['r', '3'])
+
+# b = {'b' : x}
+# z = x.union(set(['r', '3']))
+
+# print(z)
+
+# import numpy as np
+
+# a = [0, 1, 2, 3]
+
+# b = [4, 5, 6, 7]
+
+# c = [8, 9, 10, 11]
+
+# d = [a, b, c]
+
+# e = [c, b, a, a]
+
+# f = [1.5, 1.8, 2.1]
+
+# print(list(zip(list(map(lambda y, z: y * z, d, f)))))
+
+# print(list(map(lambda x: sum(x), zip(list(map(lambda y, z: y * z, d, f))))))
+
+# ret = [0] * len(a)
+# for i, col in enumerate(d):
+#     for j, cell in enumerate(col):
+#         ret[j] += cell * f[i]
+# print(ret)
+# print(np.array([np.sum(np.array([[cell * f[i] for j, cell in enumerate(col)] for i, col in enumerate(d)])[:, i]) for i in range(len(a))]))
+
+# print(list(map(lambda x: sum(x)/len(x), zip(*list(map(lambda y: list(map(lambda z: sum(y + z), list(zip(*d)))), list(zip(*e))))))))
+
+# print(list(map(lambda x: list(map(lambda y: sum(x + y) / (len(x)+len(y)), list(zip(*d)))), list(zip(*e)))))
+
+# print(list(map(lambda x: list(map(lambda y: x + y, list(zip(*d)))), list(zip(*e)))))
+
+# print(list(map(lambda x: print(x), list(zip(*d, *e)))))
+
+# print(list(map(lambda x: sum(x) / len(x), list(zip(*d, *e)))))
+
+# print(list(zip(a, b, c)))
+
+# arr1 = list(map(lambda x: sum(x), list(zip(*d))))
+# arr2 = list(map(lambda x: sum(x) / len(x), list(zip(*d))))
+# arr3 = list(map(lambda x: max(x), list(zip(*d))))
+
+# # arr4 = list(map(lambda x, y: print(x + ' ' + y), list(zip(*d)), list(zip(*e))))
+
+# print(arr1)
+# print(arr2)
+# print(arr3)
+# print(arr4)
+
+
+# test = {
+#     're' : [2, 3, 4],
+#     'te' : [6, 8, 9],
+#     'as' : [6, 2, 8]
+# }
+
+# all_tests = {'re', 'te', 'e'}
+
+
+# print(list(filter(lambda x: x in test.keys(), all_tests)))
+
+# print(list(map(lambda x: test[x], list(filter(lambda x: x in test.keys(), all_tests)))))
+
+
+import wikipedia
+
+s = 'dasdasdsadasd'
+
+page = wikipedia.page(s)
+
+wikipedia
+
+print(s)
