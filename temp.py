5c10db11857f8e755e2170b8b86db4b4e038cff0
Berend-Jan Lange <lange.berendjan@gmail.com>
2020-04-15 20:35:00 +0200

    update

--- temp.py
+++ temp.py
@@ -4,6 +4,8 @@ from in_out import InOut as IO
 
 import pandas as pd
 
+from nltk.stem import WordNetLemmatizer
+
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -30,13 +32,21 @@ base_path_dicts = './data/dictionaries/'
 # print(len(rdf2vec_model))
 
 
-data = IO.read_json(base_path_dicts + 'current_features.json')
+# data = IO.read_json(base_path_dicts + 'current_features.json')
 
-dat = []
+# dat = []
 
-for d in data.values():
-    dat.append(d)
+# for d in data.values():
+#     dat.append(d)
 
-df = pd.DataFrame(dat)
+# df = pd.DataFrame(dat)
 
-IO.write_csv(df, base_path_dicts + 'current_dataframe.csv')
+# IO.write_csv(df, base_path_dicts + 'current_dataframe.csv')
+
+content = 'world interest rates'
+
+lemmatizer = WordNetLemmatizer()
+
+lemmatized_content = ' '.join([lemmatizer.lemmatize(word) for word in content.split()])
+
+print(lemmatized_content)
